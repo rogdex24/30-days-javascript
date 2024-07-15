a3e615ab5c421ac91a2f631d42ddd95f7dd74e32
Abu Talha <rogdex24@gmail.com>
2024-07-16 02:04:45 +0530

    day 01

---
+++ day01/index.js
@@ -0,0 +1,35 @@
+// activity 1
+console.log('\n>activity 1')
+var num = 10;
+console.log('num', num);
+
+let num2 = 20;
+console.log('num2', num2);
+
+
+// activity 2
+console.log('\n>activity 2')
+const isTrue = false;
+console.log('isTrue', isTrue);
+
+// activity 3
+console.log('\n>activity 3')
+console.log('num:', typeof num);
+console.log('isTrue:', typeof isTrue);
+const name = 'Talha'
+console.log('string:', typeof name);
+const dataObj = { name: 'Talha', age: 23 }
+console.log('dataObj:', typeof dataObj);
+const dataArr = [1, 2, 3, 4, 5]
+console.log('dataArr:', typeof dataArr);
+
+// activity 4
+console.log('\n>activity 4')
+let newVar = 23;
+console.log('initial', newVar);
+newVar = 24;
+console.log('newVar', newVar);
+
+// activity 5
+console.log('\n>activity 5')
+name = 'rogdex24'
