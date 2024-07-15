fda67bf6c407a7adc64de9eea4ca9140fd310f8f
Abu Talha <rogdex24@gmail.com>
2024-07-16 02:18:16 +0530

    day02

---
+++ day02/index.js
@@ -0,0 +1,41 @@
+// activity 1
+console.log('\n>activity 1')
+
+const num1 = 5;
+const num2 = 3;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+
+// activity 2
+console.log('\n>activity 2')
+let count = 1;
+count += 1;
+console.log(count);
+count -= 1;
+console.log(count);
+
+
+// activity 3
+console.log('\n>activity 3')
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log('simple', 4 == '4');
+console.log('deep',4 === '4');
+
+
+// activity 4
+console.log('\n>activity 4')
+console.log(false && true);
+console.log(false || true);
+console.log(!true);
+
+
+// activity 5
+console.log('\n>activity 5')
+const number = 3;
+let isPositive = number >= 0 ? true : false;
+console.log('isPositive', isPositive)
